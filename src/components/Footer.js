7bd77cb1a2b5c286217c7de44b4b565eb5607002
MahfujAhsan <ahsanmahfuj@gmail.com>
2023-05-19 05:21:53 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+import Layout from './Layout'
+
+const Footer = () => {
+    return (
+        <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light'>
+            <Layout className='py-8 flex items-center justify-between'>
+                <span>{new Date().getFullYear()} &copy; All Rights Reserved. </span>
+                <div className='flex items-center'>
+                    Build With <span className="text-primary dark:text-primaryDArk text-2xl px-1">&#9825;</span> by&nbsp; <Link
+                        href="/"
+                        className='underline underline-offset-2'
+                        target={'_blank'}
+                    >
+                        Mahfujur_Rahman
+                    </Link>
+                </div>
+                <Link
+                    href="/"
+                    target={'_blank'}
+                >Say Hello
+                </Link>
+            </Layout>
+        </footer>
+    )
+}
+
+export default Footer
