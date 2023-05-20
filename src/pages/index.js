0dc5442f11943e72ecd1746d4fa672066ab09663
MahfujAhsan <ahsanmahfuj@gmail.com>
2023-05-20 23:34:18 +0600

    responsiveness updating

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,22 +17,22 @@ export default function Home() {
         <meta name="description" content="Software Developer" />
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className='pt-0'>
-          <div className="flex items-center justify-between w-full">
-            <div className='w-1/2'>
+        <Layout className='pt-0 md:p-16 sm:p-8'>
+          <div className="flex lg:flex-col items-center justify-between w-full">
+            <div className='w-1/2 md:w-full'>
               <Image
                 src={dev}
                 alt='Developer_Image'
-                className='w-9/12 mr-auto rounded-full h-9/12'
+                className='w-9/12 mr-auto rounded-full h-9/12 lg:hidden md:inline-block md:w-full'
                 priority
                 sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
               />
             </div>
-            <div className='w-1/2 flex flex-col item-center self-center'>
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
+            <div className='w-1/2 flex flex-col item-center self-center lg:w-full lg:text-center'>
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' />
               <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                   <Link href="/dummy.pdf" target={"_blank"}
                   className='flex items-center bg-[#303F60] text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-[#C62228] dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light'
                   download={true}
